2a76bcbee41a3177e2a57628cc9415480ce04231
Travis CI <najwer23@gmail.com>
2021-09-30 00:10:47 +0200

    Reconfigured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
+
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: [
-      '@babel/polyfill',
-      './assets/js/index.js'
-    ],
-    styleMain: [
-      './assets/js/style.js'
-    ]
+    index: path.resolve(__dirname, './assets/js/index.js'),
+    style: path.resolve(__dirname, './assets/js/style.js') 
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].bunde.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: "./assets/img/f.png",
+    })
+  ],
   module: {
     rules: [
       {
@@ -27,19 +29,63 @@ module.exports = {
         }
       },
       {
-        test: /\.(sass|scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader']
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ['file-loader']
-      }
+        test: /\.(sass|scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(?:ico|gif|svg|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
     ]
-  }
+  },
 }
 
-// https://github.com/webpack/webpack/issues/1732
+
+
+
+
+
+
+
+
+
+// const path = require('path')
+
+// module.exports = {
+//   entry: {
+//     app: path.resolve(__dirname, './assets/js/index.js'),
+//     styleMain: path.resolve(__dirname, './assets/js/style.js'),
+//   },
+//   output: {
+//     path: path.resolve(__dirname, './build'),
+//     filename: '[name].js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(sass|scss|css)$/,
+//         use: ['style-loader', 'css-loader', 'sass-loader']
+//       },
+//       {
+//         test: /\.(woff|woff2|eot|ttf|otf)$/,
+//         use: ['file-loader']
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif)$/,
+//         use: ['file-loader']
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: path.resolve(__dirname, 'index.html'),
+//       inject: false 
+//     })
+//   ]
+// }
+
+// // https://github.com/webpack/webpack/issues/1732
